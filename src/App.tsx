56437d14a66379974431866098d302c1264dcd9f
slavaMatyuk <vyacheslav.matyuk@innowise-group.com>
2022-09-07 16:07:21 +0300

    [develop]: structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>CIS Holder</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
