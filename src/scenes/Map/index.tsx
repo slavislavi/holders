48a55b78f75ffcd74877b5e893159139e722e878
slavaMatyuk <vyacheslav.matyuk@innowise-group.com>
2022-09-08 13:29:13 +0300

    [develop]: add-mapscreen-template

---
+++ src/scenes/Map/index.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+import {View, Text} from 'react-native';
+import {styles} from './styles';
+
+export const Map: FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>MAP</Text>
+    </View>
+  );
+};
---
+++ src/scenes/Map/styles.ts
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#afafcf',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    color: '#335555',
+    paddingHorizontal: 25,
+    paddingVertical: 5,
+  },
+});
