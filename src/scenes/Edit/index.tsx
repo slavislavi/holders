c4005792574a5f3e004683a7f0018ac7b5cac35c
slavaMatyuk <vyacheslav.matyuk@innowise-group.com>
2022-09-19 18:29:49 +0300

    [develop]: template-of-input-forms

--- src/scenes/Edit/index.tsx
+++ src/scenes/Edit/index.tsx
@@ -1,11 +1,72 @@
 import React, {FC} from 'react';
-import {View, Text} from 'react-native';
+import {ScrollView, TextInput, TouchableOpacity} from 'react-native';
+import {useForm, Controller} from 'react-hook-form';
+import {CustomText} from '@components/CustomText';
 import {styles} from './styles';
 
 export const Edit: FC = () => {
+  const {control, handleSubmit} = useForm();
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>EDIT</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <CustomText style={styles.inputTitle}>fill the forms</CustomText>
+
+      <Controller
+        control={control}
+        name='name'
+        render={({field: {value, onChange}}) => (
+          <TextInput
+            placeholder='Name'
+            value={value}
+            onChangeText={onChange}
+            // inlineImageLeft='search_icon'
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name='type'
+        render={({field: {value, onChange}}) => (
+          <TextInput
+            placeholder='Type'
+            value={value}
+            onChangeText={onChange}
+            // inlineImageLeft='search_icon'
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name='address'
+        render={({field: {value, onChange}}) => (
+          <TextInput
+            placeholder='Address'
+            value={value}
+            onChangeText={onChange}
+            // inlineImageLeft='search_icon'
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name='description'
+        render={({field: {value, onChange}}) => (
+          <TextInput
+            multiline
+            placeholder='Description'
+            value={value}
+            onChangeText={onChange}
+            // inlineImageLeft='search_icon'
+          />
+        )}
+      />
+      <TextInput placeholder='File' />
+
+      <TouchableOpacity style={styles.submitButton}>
+        <CustomText style={styles.submitButtonText}>add new</CustomText>
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
--- src/scenes/Edit/styles.ts
+++ src/scenes/Edit/styles.ts
@@ -5,15 +5,25 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: THEME.APP_BG,
-    justifyContent: 'center',
     alignItems: 'center',
   },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
+  inputTitle: {
+    fontSize: 36,
     textTransform: 'uppercase',
     color: THEME.PRIMARY,
     paddingHorizontal: 25,
     paddingVertical: 5,
+  },
+  submitButton: {
+    backgroundColor: THEME.DANGER,
+    borderRadius: 5,
+    height: 50,
+    justifyContent: 'center',
+  },
+  submitButtonText: {
+    color: THEME.SECONDARY,
+    fontSize: 36,
+    textTransform: 'uppercase',
+    paddingHorizontal: 36,
   },
 });
